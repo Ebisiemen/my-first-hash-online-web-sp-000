6fe172d56246a84143e66b1b99bab2346f656e33
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-10-24 17:52:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,20 +22,20 @@ end
 
 
 
-# def adding
-#   shipping_manifest = {
-#                   "whale bone corsets" => 5, 
-#                   "porcelain vases" => 2, 
-#                   "oil paintings" => 3
-#                 }
-#   # add 2 muskets to the shipping_manifest hash below
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                }
+  # add 2 muskets to the shipping_manifest hash below
 
 
 
-#   # add 4 gun powder to the shipping_manifest hash below
+  # add 4 gun powder to the shipping_manifest hash below
 
 
 
-#   # return the shipping_manifest hash below
+  # return the shipping_manifest hash below
 
-# end
+end
